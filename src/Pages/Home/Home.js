cc89c4328e4237678108916ea7675a7a233d9d2f
afahnyu_pila05 <fulopila9@gmail.com>
2023-09-17 19:14:44 -0400

    refactor home-page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -8,33 +8,83 @@ import {
   getArrivalItemsService,
   getPopularItemsService,
 } from "../../Services/HomeService/HomeService";
+import UseAnimations from "react-useanimations";
+import loading from "react-useanimations/lib/loading";
 
 const Home = () => {
-  const { data: arrivalProducts = [] } = useQuery("arrivalQuery", () =>
-    getArrivalItemsService()
-  );
-  const { data: popularProducts = [] } = useQuery("popularQuery", () =>
-    getPopularItemsService()
-  );
+  const {
+    data: arrivalProducts = [],
+    isFetching: fetchingArrival,
+    isError: arrivalIsError,
+    error: arrivalError,
+    refetch: refetchArrival,
+  } = useQuery("arrivalQuery", () => getArrivalItemsService());
+  const {
+    data: popularProducts = [],
+    isFetching: fetchingPopular,
+    error: popularError,
+    isError: popularIsError,
+    refetch: refetchPopular,
+  } = useQuery("popularQuery", () => getPopularItemsService());
+
+  let arrivalContent;
+  let popularContent;
+
+  if (fetchingArrival) {
+    arrivalContent = <UseAnimations animation={loading} />;
+  } else if (arrivalIsError) {
+    arrivalContent = (
+      <div>
+        <p className="text-red-500">{arrivalError.message}</p>
+        <button onClick={() => refetchArrival()}>
+          refetch new arrival products...
+        </button>
+      </div>
+    );
+  } else {
+    arrivalContent = (
+      <div>
+        {arrivalProducts.map((arrival) => (
+          <NewArrivals
+            key={arrival.id}
+            arrivalData={arrival}
+          />
+        ))}
+      </div>
+    );
+  }
+
+  if (fetchingPopular) {
+    popularContent = <UseAnimations animation={loading} />;
+  } else if (popularIsError) {
+    popularContent = (
+      <div>
+        <p className="text-red-500">{popularError.message}</p>
+        <button onClick={() => refetchPopular()}>
+          refetch popular products...
+        </button>
+      </div>
+    );
+  } else {
+    popularContent = (
+      <div>
+        {popularProducts.map((popular) => (
+          <PopularItemsCard key={popular.id} popularData={popular} />
+        ))}
+      </div>
+    );
+  }
 
   return (
     <React.Fragment>
       <Banner />
 
       <h3>Arrival product list</h3>
-      {arrivalProducts.map((arrival) => (
-        <NewArrivals
-          key={arrival.id}
-          arrivalData={arrival}
-          // refetchArrival={refetchArrival}
-        />
-      ))}
+      {arrivalContent}
       <FinePens />
 
       <h3>Popular product list</h3>
-      {popularProducts.map((popular) => (
-        <PopularItemsCard key={popular.id} popularData={popular} />
-      ))}
+      {popularContent}
     </React.Fragment>
   );
 };
