66c0d7b5a565f032ac1f252e37eee22d577d5560
afahnyu_pila05 <fulopila9@gmail.com>
2023-10-05 04:10:01 -0400

    refactor authButton

--- src/Pages/Home/Buttons/AuthButton.js
+++ src/Pages/Home/Buttons/AuthButton.js
@@ -17,14 +17,12 @@ export default function LanguageButton() {
         <Menu.Button
           className="
         inline-flex w-full justify-center 
-        gap-x-1.5 rounded-md bg-white 
-        px-5 py-2 text-sm font-semibold 
-        text-gray-900 shadow-sm ring-inset 
-        ring-gray-300 hover:bg-gray-50
+        text-white 
+        text-lg 
         "
         >
           {t("auth.signIn")}
-          <IconName icon={ic_keyboard_arrow_down_twotone} />
+          <IconName icon={ic_keyboard_arrow_down_twotone} style={{marginLeft: '.5rem'}} />
         </Menu.Button>
       </div>
 
