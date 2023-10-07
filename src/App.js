f4e04cf0e8d5738c2e0b118e38a0025298092af5
afahnyu_pila05 <fulopila9@gmail.com>
2023-10-07 08:29:39 -0400

    restart building new platform from scratch

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
-// TODO: Try implementing Custom Input and fetch-post hooks in the Checkout form and other places where possible.
-
-import React, { useState } from "react";
-import { useRoutes } from "react-router-dom";
-
-import Header from "./Components/Layout/Header.js";
-import FooterNavbar from "./Components/Layout/FooterNavbar";
-import { routes } from "./Routes/routes.js";
-import { QueryClientProvider, QueryClient } from "react-query";
-import Login from "./Pages/Auth/Auth.js";
-import Cart from "./Components/Cart/Cart.js";
+import React from "react";
 
 function App() {
-  // const queryClient = new QueryClient()
-
-  // TODO: HANDLE THE LOGIN && SIGNUP STATES HERE TO SWITCH BETWEEN FORMS.
-  // TODO: REMOVE CART FROM MODAL TO PAGE.
-  const [showAuthModal, setShowAuthModal] = useState(false);
-
-  const handleShowAuthModal = () => setShowAuthModal(!showAuthModal)
-
-  const [userName, setUserName] = useState("")
-
-  const handleUserName = e => setUserName(e.target.value)
-
-  // useRoutes Navigation
-  const route = useRoutes(routes);
 
   return (
     <React.StrictMode>
-      <QueryClientProvider client={new QueryClient()}>
-        {showAuthModal && <Login userName={userName} handleUserName={handleUserName} onHideAuthModal={handleShowAuthModal} />}
-
-        <Header userName={userName} onShowAuthModal={handleShowAuthModal} showAuthModal={showAuthModal} />
-
-        {/* <Cart /> */}
-
-        {route}
-
-        <FooterNavbar />
-      </QueryClientProvider>
+      <h1>Hello Pila, Please start all over. Thank you.</h1>
     </React.StrictMode>
   );
 }
