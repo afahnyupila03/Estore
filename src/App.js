ac392390eb2b1d7f78f552ff6bc71490c671d63b
afahnyu_pila05 <fulopila9@gmail.com>
2023-10-07 09:03:32 -0400

    test merge

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <React.StrictMode>
       <h1>Hello Pila, Please start all over. Thank you.</h1>
+      <p>Testing merging to maiin branch to see if it works.</p>
     </React.StrictMode>
   );
 }
