972690a648d052c7767639d99c2f068f9e69d1e7
Afahnyu Pila <AfahnyuPila03>
2023-12-17 22:38:18 +0100

    create fetch api calls for various product categories

---
+++ src/Services/CategoryService.js
@@ -0,0 +1,84 @@
+// https://fakestoreapi.com/products/category/jewelery
+
+export const JewelryCategory = async () => {
+  try {
+    const response = await fetch(
+      "https://fakestoreapi.com/products/category/jewelery"
+    );
+    const data = await response.json();
+    const jewelryItems = [];
+    for (const jewelryKey in data) {
+      jewelryItems.push({
+        id: jewelryKey,
+        title: data[jewelryKey].title,
+        price: data[jewelryKey].price,
+      });
+    }
+    return jewelryItems;
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+export const ElectronicCategory = async () => {
+  try {
+    const response = await fetch(
+      "https://fakestoreapi.com/products/category/electronic"
+    );
+    const data = await response.json();
+    const electronicItems = [];
+    for (const electronicItemsKey in data) {
+      electronicItems.push({
+        id: electronicItemsKey,
+        title: data[electronicItemsKey].title,
+        image: data[electronicItemsKey].image,
+        price: data[electronicItemsKey].price,
+      });
+    }
+    return electronicItems;
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+export const MenClothingCategory = async () => {
+  try {
+    const response = await fetch(
+      "https://fakestoreapi.com/products/category/men"
+    );
+    const data = await response.json();
+    const menItems = [];
+    for (const mensItemKey in data) {
+      menItems.push({
+        id: mensItemKey,
+        title: data[mensItemKey].title,
+        image: data[mensItemKey].image,
+        price: data[mensItemKey].price,
+      });
+    }
+    return menItems;
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+export const WomenClothingCategory = async () => {
+  try {
+    const response = await fetch(
+      "https://fakestoreapi.com/products/category/women"
+    );
+    const data = await response.json();
+    const womenItems = [];
+    for (const womenItemsKey in data) {
+      womenItems.push({
+        id: womenItemsKey,
+        title: data[womenItemsKey].title,
+        image: data[womenItemsKey].image,
+        price: data[womenItemsKey].price,
+      });
+    }
+    return womenItems;
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
