29888099e92f7ba1e08ab9edfaf7f0048fbc4343
Afahnyu Pila <AfahnyuPila03>
2024-01-21 23:34:42 +0100

    implement blog-dummy fetch api

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,5 +1,34 @@
+import { useQuery } from "react-query";
+import { BlogServices } from "../../Services/BlogService";
+import BlogCard from "./Components/BlogCard";
+
 const Blog = (props) => {
-  return <h1 className="mt-40">Blog Page</h1>;
+  const {
+    data = [],
+    isLoading,
+    isError,
+    error,
+  } = useQuery("blogPost", () => BlogServices());
+  console.log("Blog Page:", data)
+
+  let BlogPost;
+
+  if (isLoading) {
+    BlogPost = <p>Loading....!</p>;
+  } else if (isError) {
+    BlogPost = <p>{error} Something went wrong</p>;
+  } else {
+    BlogPost = (
+      <BlogCard blogData={data} />
+    );
+  }
+
+  return (
+    <div>
+      <h1 className="mt-40">Blog Page</h1>
+      {BlogPost}
+    </div>
+  );
 };
 
 export default Blog;
---
+++ src/Services/BlogService.js
@@ -0,0 +1,29 @@
+export const BlogServices = async () => {
+  try {
+    const res = await fetch(
+      "https://api.slingacademy.com/v1/sample-data/blog-posts?offset=5&limit=30"
+    );
+    const data = await res.json();
+    const blogPost = data.blogs;
+    console.log("Service: ", blogPost);
+    const blogData = [];
+    for (const key in blogPost) {
+      if (blogPost.hasOwnProperty(key)) {
+        blogData.push({
+          userId: blogPost[key].userId,
+          title: blogPost[key].title,
+          content: blogPost[key].content_text,
+          photo: blogPost[key].photo_url,
+          createAt: blogPost[key].create_at,
+          id: blogPost[key].id,
+          description: blogPost[key].description,
+          category: blogPost[key].category,
+          updatedAt: blogPost[key].updated_at,
+        });
+      }
+    }
+    return blogData;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
