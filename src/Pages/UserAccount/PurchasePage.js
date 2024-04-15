a4dd71f6a4ca2e2a11a4f8a6bb2bbbf5337607d3
Afahnyu Pila <AfahnyuPila03>
2024-04-15 22:14:52 +0200

    fix purchase invoice render method and add navigation to single invoice

--- src/Pages/UserAccount/PurchasePage.js
+++ src/Pages/UserAccount/PurchasePage.js
@@ -42,6 +42,19 @@ export default function PurchasePage() {
     return formatter.format(amount);
   };
 
+  function CONVERT_CURRENCY(priceInUSD) {
+    const exchangeRate = 608.58;
+    const convertedPrice = Math.round(priceInUSD * exchangeRate);
+    const discount = convertedPrice;
+    return discount;
+  }
+
+  function DISCOUNT_PRICE(discountPercentage, price) {
+    const discount = (discountPercentage / 100) * price;
+    const discountedPrice = Math.round(price - discount);
+    return discountedPrice;
+  }
+
   let purchase;
 
   if (user === null) {
@@ -88,110 +101,227 @@ export default function PurchasePage() {
   } else {
     purchase = (
       <div>
-        {data.map((purchase) => {
-          const {
-            id,
-            productData,
-            productQuantity,
-            tax,
-            checkoutTotal,
-            shippingPrice,
-            purchaseId,
-            email,
-            displayName,
-            address,
-            city,
-            state,
-            tel,
-            cardNumber,
-            timeOfOrder,
-            dayOfOrder,
-          } = purchase;
+        {data.length > 3
+          ? data.slice(-2).map((purchase) => {
+              const {
+                id,
+                productData,
+                productQuantity,
+                tax,
+                checkoutTotal,
+                shippingPrice,
+                purchaseId,
+                email,
+                displayName,
+                address,
+                city,
+                state,
+                tel,
+                cardNumber,
+                timeOfOrder,
+                dayOfOrder,
+              } = purchase;
 
-          return (
-            // <div key={id}>
-            <table
-              key={id}
-              // style={TABLE_STYLES.table}
-              className="rounded-lg p-8 border-2 border-red"
-            >
-              <tr
-                style={TABLE_STYLES.purchaseInfo}
-                className="bg-gray-300 text-center"
-              >
-                <th>Date placed</th>
-                <th>Invoice N°</th>
-                <th>Tax</th>
-                <th>Shipping</th>
-                <th>Total amount</th>
-              </tr>
-              <tr>
-                <td>
-                  {dayOfOrder} {timeOfOrder}
-                </td>
-                <td>{purchaseId}</td>
-                <td className="text-center">
-                  {FORMAT_MONEY(parseInt(tax), CURRENCY)}
-                </td>
-                <td className="text-center">
-                  {FORMAT_MONEY(parseInt(shippingPrice), CURRENCY)}
-                </td>
-                <td className="text-center">
-                  {FORMAT_MONEY(parseInt(checkoutTotal), CURRENCY)}
-                </td>
-              </tr>
-              <tr className="bg-gray-300">
-                <th>Product</th>
-                <th>Brand</th>
-                <th>Price</th>
-                <th>Status</th>
-                <th>Info</th>
-              </tr>
-              {productData.map((item) =>
-                Object.keys(item).map((key) => (
-                  <tr key={key}>
-                    <td>
-                      {item[key].title} x {item[key].quantity}
-                    </td>
-                    <td className="text-center">{item[key].brand}</td>
-                    <td className="text-center">
-                      {FORMAT_MONEY(item[key].price, CURRENCY)}
-                    </td>
-                    <td className="text-center">En-route</td>
-                    <td className="text-center">View</td>
+              return (
+                // <div key={id}>
+                <table
+                  key={id}
+                  // style={TABLE_STYLES.table}
+                  className="rounded-lg text-center w-full border-2 border-black"
+                >
+                  <tr
+                    style={TABLE_STYLES.purchaseInfo}
+                    className="bg-gray-300 p-8 text-center"
+                  >
+                    <th>Date placed</th>
+                    <th>Invoice N°</th>
+                    <th>Tax</th>
+                    <th>Shipping</th>
+                    <th>Total amount</th>
                   </tr>
-                ))
-              )}
-              <tr className="bg-gray-300 text-center">
-                <th>User Infor</th>
-                <th>Delivery Info</th>
-                <th>Payment Info</th>
-                <th>Prod Quant </th>
-                <th>INVOICE</th>
-              </tr>
-              <tr>
-                <td>Email: {email}</td>
-                <td>
-                  {city}, {state}
-                </td>
-                <td>ends with ({cardNumber})</td>
-                <td>{productQuantity}</td>
+                  <tr>
+                    <td>
+                      {dayOfOrder} {timeOfOrder}
+                    </td>
+                    <td>{purchaseId}</td>
+                    <td className="text-center">
+                      {FORMAT_MONEY(parseInt(tax), CURRENCY)}
+                    </td>
+                    <td className="text-center">
+                      {FORMAT_MONEY(parseInt(shippingPrice), CURRENCY)}
+                    </td>
+                    <td className="text-center">
+                      {FORMAT_MONEY(parseInt(checkoutTotal), CURRENCY)}
+                    </td>
+                  </tr>
+                  <tr className="bg-gray-300">
+                    <th>Product</th>
+                    <th>Brand</th>
+                    <th>Price</th>
+                    <th>Status</th>
+                    <th>Info</th>
+                  </tr>
+                  {productData.map((item) =>
+                    Object.keys(item).map((key) => (
+                      <tr key={key}>
+                        <td>
+                          {getName(item[key].title)} x {item[key].quantity}
+                        </td>
+                        <td className="text-center">{item[key].brand}</td>
+                        <td className="text-center">
+                          {FORMAT_MONEY(item[key].price, CURRENCY)}
+                        </td>
+                        <td className="text-center">En-route</td>
+                        <td className="text-center">View</td>
+                      </tr>
+                    ))
+                  )}
+                  <tr className="bg-gray-300 text-center">
+                    <th>User Infor</th>
+                    <th>Delivery Info</th>
+                    <th>Payment Info</th>
+                    <th>Prod Quant </th>
+                    <th>INVOICE</th>
+                  </tr>
+                  <tr>
+                    <td>Email: {email}</td>
+                    <td>
+                      {city}, {state}
+                    </td>
+                    <td>ends with ({HIDE_CARD_NUMBER(cardNumber)})</td>
+                    <td className="text-center">{productQuantity}</td>
 
-                <td className="text-center">VIEW INVOICE</td>
-              </tr>
-              <tr>
-                <td>Name: {displayName}</td>
-                <td>{address}</td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>Tel: {tel}</td>
-              </tr>
-            </table>
-            // </div>
-          );
-        })}
+                    <td className="text-center">VIEW INVOICE</td>
+                  </tr>
+                  <tr>
+                    <td>Name: {displayName}</td>
+                    <td>{address}</td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>Tel: {tel}</td>
+                  </tr>
+                </table>
+                // </div>
+              );
+            })
+          : data.map((purchase) => {
+              const {
+                id,
+                productData,
+                productQuantity,
+                tax,
+                checkoutTotal,
+                shippingPrice,
+                purchaseId,
+                email,
+                displayName,
+                address,
+                city,
+                state,
+                tel,
+                cardNumber,
+                timeOfOrder,
+                dayOfOrder,
+              } = purchase;
+
+              return (
+                <div className="border-1 border-black rounded-lg" key={id}>
+                  <table
+                    key={id}
+                    className="rounded-lg w-full border-2 border-black"
+                  >
+                    <tr
+                      style={TABLE_STYLES.purchaseInfo}
+                      className="bg-gray-300 p-8 text-center"
+                    >
+                      <th>Date placed</th>
+                      <th>Invoice N°</th>
+                      <th>Tax</th>
+                      <th>Shipping</th>
+                      <th>Total amount</th>
+                    </tr>
+                    <tr>
+                      <td>
+                        {dayOfOrder} {timeOfOrder}
+                      </td>
+                      <td>{purchaseId}</td>
+                      <td className="text-center">
+                        {FORMAT_MONEY(parseInt(tax), CURRENCY)}
+                      </td>
+                      <td className="text-center">
+                        {FORMAT_MONEY(parseInt(shippingPrice), CURRENCY)}
+                      </td>
+                      <td className="text-center">
+                        {FORMAT_MONEY(parseInt(checkoutTotal), CURRENCY)}
+                      </td>
+                    </tr>
+                    <tr className="bg-gray-300">
+                      <th>Product</th>
+                      <th>Brand</th>
+                      <th>Price</th>
+                      <th>Status</th>
+                      <th>Info</th>
+                    </tr>
+                    {productData.map((item) =>
+                      Object.keys(item).map((key) => (
+                        <tr key={key}>
+                          <td>
+                            {getName(item[key].title)} x {item[key].quantity}
+                          </td>
+                          <td className="text-center">{item[key].brand}</td>
+                          <td className="text-center">
+                            {FORMAT_MONEY(
+                              DISCOUNT_PRICE(
+                                item[key].discountPercentage,
+                                CONVERT_CURRENCY(item[key].price)
+                              ),
+                              CURRENCY
+                            )}
+                          </td>
+                          <td className="text-center">En-route...</td>
+                          <td className="text-center text-gray-800">View</td>
+                        </tr>
+                      ))
+                    )}
+                    <tr className="bg-gray-300 text-center">
+                      <th>User Infor</th>
+                      <th>Delivery Info</th>
+                      <th>Payment Info</th>
+                      <th>Prod Quant </th>
+                      <th>INVOICE</th>
+                    </tr>
+                    <tr>
+                      <td>{email}</td>
+                      <td>
+                        {city}, {state}
+                      </td>
+                      <td>ends with ({HIDE_CARD_NUMBER(cardNumber)})</td>
+                      <td className="text-center">{productQuantity}</td>
+                      <td className="text-center">
+                        <Link
+                          target="_blank"
+                          className="text-gray-800"
+                          to={`/purchase/${purchaseId}`}
+                        >
+                          View invoice
+                        </Link>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>{displayName}</td>
+                      <td>{address}</td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td> {tel}</td>
+                    </tr>
+                  </table>
+                </div>
+              );
+            })}
       </div>
     );
   }
@@ -203,7 +333,24 @@ export default function PurchasePage() {
           Purchases <span>({data.length})</span>
         </h1>
       </div>
-      <div>{purchase}</div>
+      <div className="mt-2">{purchase}</div>
     </Fragment>
   );
 }
+
+function HIDE_CARD_NUMBER(cardNumber) {
+  if (cardNumber.length === 16) {
+    const hiddenDigits = cardNumber.substring(12);
+    return hiddenDigits;
+  } else {
+    return "Invalid card number";
+  }
+}
+
+const getName = (title) => {
+  const MAX_TITLE_CHARS = 20;
+  if (title.length > MAX_TITLE_CHARS) {
+    return `${title.slice(0, MAX_TITLE_CHARS)}...`;
+  }
+  return title;
+};
