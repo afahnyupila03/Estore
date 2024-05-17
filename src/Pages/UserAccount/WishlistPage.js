d5f0e727b0ec98f34b3fdf7e9dba5326187b5a3a
Afahnyu Pila <AfahnyuPila03>
2024-05-17 18:47:21 +0200

    fix wishListedProduct rendering in WishListPage

--- src/Pages/UserAccount/WishlistPage.js
+++ src/Pages/UserAccount/WishlistPage.js
@@ -1,21 +1,51 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { IonIcon } from "@ionic/react";
 import { shareOutline } from "ionicons/icons";
 import { Link } from "react-router-dom";
 import { useAuth, useCart, useWishList } from "../../Store";
 import WishListCardItems from "./Components/CardComponents/WishListCardItems";
+import { remove } from "firebase/database";
 
 export default function WishlistPage() {
-  const {
-    wishListProducts,
-    wishListQuantity,
-    wishListed,
-    removeProductFromWishList,
-    clearWishListHandler,
-  } = useWishList();
+  const { removeProductFromWishList } = useWishList();
   const { user } = useAuth();
   const { addProductHandler } = useCart();
 
+  const [wishListProducts, setWishListProducts] = useState([]);
+
+  const handleDislikeWishListedProducts = (id) => {
+    removeProductFromWishList(id);
+    setWishListProducts((prevProducts) =>
+      prevProducts.filter((products) => products.id !== id)
+    );
+
+    const storedWishListData =
+      JSON.parse(sessionStorage.getItem("wishListData")) || [];
+    const updatedWishListData = storedWishListData.filter(
+      (product) => product.id !== id
+    );
+    sessionStorage.setItem("wishListData", JSON.stringify(updatedWishListData));
+  };
+
+  const getAllLocalStorageData = () => {
+    const allData = {};
+    for (let i = 0; i < sessionStorage.length; i++) {
+      const key = sessionStorage.key(i);
+      // Exclude the language key from the retrieved data
+      if (key !== "i18nextLng") {
+        const value = sessionStorage.getItem(key);
+        allData[key] = JSON.parse(value);
+      }
+    }
+    return allData;
+  };
+
+  useEffect(() => {
+    const sessionStorageData = getAllLocalStorageData();
+    const products = sessionStorageData["wishListData"] || [];
+    setWishListProducts(products);
+    setWishListQuantity(products.length);
+  }, []);
 
   let wishlist;
 
@@ -52,11 +82,10 @@ export default function WishlistPage() {
           <WishListCardItems
             key={wishListProducts.id}
             removeItemHandler={() =>
-              removeProductFromWishList(wishListProducts.id)
+              handleDislikeWishListedProducts(wishListProducts.id)
             }
             addItemHandler={() => addProductHandler(wishListProducts)}
             wishListProducts={wishListProducts}
-            wishListed={wishListed}
           />
         ))}
       </div>
@@ -68,7 +97,7 @@ export default function WishlistPage() {
       <div className="flex justify-between items-center">
         <h1 className="text-2xl font-semibold font-mono">
           Wish List
-          <span>({wishListQuantity})</span>
+          <span>({wishListProducts.length})</span>
         </h1>
         {user !== null && (
           <div className="flex items-center">
