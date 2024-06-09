2afbfc5b44e6a72a10ed03645bbcca8f43351ee8
Afahnyu Pila <AfahnyuPila03>
2024-06-09 12:28:31 +0200

    add react-flowbite dependency, in ShopPage component, implement SideBar-Component and render prodcut categories

--- src/Pages/Home/components/LayoutNavigation.js
+++ src/Pages/Home/components/LayoutNavigation.js
@@ -18,7 +18,7 @@ export const NavbarRoutes = (t) => [
   {
     navLink: t("navbar.blog"),
     navRoute: "blog",
-  }
+  },
 ];
 
 export const AuthRoute = (t) => [
@@ -127,4 +127,117 @@ export const AboutRoutes = (t) => [
   { navLink: t("home.timezoneBlog"), navRoute: "blog" },
   { navLink: t("home.theThread"), navRoute: "thread" },
   { navLink: t("home.timezonePodcast"), navRoute: "podcast" },
+];
+
+export const AccessoriesRoute = (t, kitchen, mobile, sports) => [
+  {
+    icon: kitchen,
+    name: t("category.kitchenAccess"),
+    route: "/kitchen-accessories",
+  },
+  {
+    icon: mobile,
+    name: t("category.mobileAccess"),
+    route: "/mobile-accessories",
+  },
+  {
+    icon: sports,
+    name: t("category.sportsAccess"),
+    route: "/sport-accessories",
+  },
+];
+
+export const AutoMobileRoutes = (t, vehicle, bike) => [
+  {
+    icon: bike,
+    name: t("category.motorcycle"),
+    route: "/motorcycles",
+  },
+  {
+    icon: vehicle,
+    name: t("category.vehicle"),
+    route: "/vehicle",
+  },
+];
+
+export const ElectronicRoutes = (t, laptop, smartphone, tablet) => [
+  {
+    icon: laptop,
+    name: t("category.laptops"),
+    route: "/laptops",
+  },
+  {
+    icon: smartphone,
+    name: t("category.smartphones"),
+    route: "/smartphones",
+  },
+  {
+    icon: tablet,
+    name: t("category.tablet"),
+    route: "/tablets",
+  },
+];
+
+export const MenRoutes = (t, footwear, shirts, watches) => [
+  {
+    icon: footwear,
+    name: t("category.menFoot"),
+    route: "/men-footwear",
+  },
+  {
+    icon: shirts,
+    name: t("category.menShirts"),
+    route: "/men-shirts",
+  },
+  {
+    icon: watches,
+    name: t("category.menWatches"),
+    route: "/men-watches",
+  },
+];
+
+export const WomenRoutes = (
+  t,
+  beauty,
+  bags,
+  cloths,
+  footwear,
+  jewelry,
+  watches
+) => [
+  {
+    icon: beauty,
+    name: t("category.beauty"),
+    route: "/beauty",
+  },
+  {
+    icon: bags,
+    name: t("category.womenBags"),
+    route: "/women-bags",
+  },
+  {
+    icon: cloths,
+    name: t("category.womenCloths"),
+    route: "/women-cloths",
+  },
+  {
+    icon: footwear,
+    name: t("category.womenFoot"),
+    route: "/women-footwear",
+  },
+  {
+    icon: jewelry,
+    name: t("category.womenJewelry"),
+    route: "/women-jewelry",
+  },
+  {
+    icon: cloths,
+    name: t("category.tops"),
+    route: "/tops",
+  },
+  {
+    icon: watches,
+    name: t("category.womenWatches"),
+    route: "/women-watches",
+  },
 ];
--- src/Pages/Shop/Shop.js
+++ src/Pages/Shop/Shop.js
@@ -1,47 +1,322 @@
 import React from "react";
 import { useQuery } from "react-query";
-import UseAnimation from "../../Components/Loader"
-import loading from "react-useanimations/lib/loading"
+import UseAnimation from "../../Components/Loader";
+import loading from "react-useanimations/lib/loading";
 import { ShopProductsService } from "../../Services/ShopService";
 import { Link } from "react-router-dom";
 import ProductItemCard from "../../Components/ProductItemCard";
 
-const Shop = (props) => {
-  const { data = {}, isLoading, isError, error, refetch } = useQuery("shopProducts", () =>
-    ShopProductsService()
+import { Sidebar } from "flowbite-react";
+import { FaSprayCan } from "react-icons/fa";
+import { GiWoodenChair } from "react-icons/gi";
+import { MdLocalGroceryStore } from "react-icons/md";
+import {
+  IoHome,
+  IoGlasses,
+  IoFitness,
+  IoCarSport,
+  IoLaptop,
+  IoTabletLandscape,
+  IoWatchSharp,
+  IoSparkles,
+} from "react-icons/io5";
+import {
+  FaHandSparkles,
+  FaKitchenSet,
+  FaMobile,
+  FaMotorcycle,
+  FaShoePrints,
+  FaShirt,
+  FaBagShopping,
+} from "react-icons/fa6";
+import { GiNecklaceDisplay } from "react-icons/gi";
+
+import { HiOutlinePlusSm, HiOutlineMinusSm } from "react-icons/hi";
+import { twMerge } from "tailwind-merge";
+import {
+  AccessoriesRoute,
+  AutoMobileRoutes,
+  ElectronicRoutes,
+  MenRoutes,
+  WomenRoutes,
+} from "../Home/components/LayoutNavigation";
+import { useTranslation } from "react-i18next";
+
+const Shop = () => {
+  const { t } = useTranslation();
+
+  const {
+    data = {},
+    isLoading,
+    isError,
+    error,
+    refetch,
+  } = useQuery("shopProducts", () => ShopProductsService());
+  console.log(data);
+
+  const Accessories = AccessoriesRoute(t, FaKitchenSet, FaMobile, IoFitness);
+  const AutoMobile = AutoMobileRoutes(t, IoCarSport, FaMotorcycle);
+  const Electronics = ElectronicRoutes(
+    t,
+    IoLaptop,
+    IoTabletLandscape,
+    FaMobile
   );
-  console.log(data)
+  const Men = MenRoutes(t, IoWatchSharp, FaShoePrints, FaShirt);
+  const Women = WomenRoutes(
+    t,
+    IoSparkles,
+    FaBagShopping,
+    FaShirt,
+    FaShoePrints,
+    GiNecklaceDisplay,
+    IoWatchSharp
+  );
 
   let shopProducts;
 
-  if(isLoading) {
-    shopProducts = (
-      <UseAnimation animation={loading} size={60} />
-    )
-  } else if(isError) {
+  if (isLoading) {
+    shopProducts = <UseAnimation animation={loading} size={60} />;
+  } else if (isError) {
     shopProducts = (
       <div>
-      <p>{error}</p>
-      <button onClick={() => refetch()}>Try again</button>
-    </div>
-    )
+        <p>{error}</p>
+        <button onClick={() => refetch()}>Try again</button>
+      </div>
+    );
   } else {
     shopProducts = (
       <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         {data.map((shopItems) => (
-          <ProductItemCard 
-          productData= {shopItems} key={shopItems.id}/>
+          <ProductItemCard productData={shopItems} key={shopItems.id} />
         ))}
       </div>
-    )
+    );
   }
 
+  const iconSize = 20;
+  const classStyle = "mr-4";
+  const divClass = "flex items-center";
+  const sideBarClass = "text-lg";
+
   return (
-    <React.Fragment>
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        {shopProducts}
+    <div className="flex items-center">
+      <Sidebar aria-label="Sidebar with content separator example">
+        <Sidebar.Items>
+          <Sidebar.ItemGroup>
+            <Sidebar.Collapse
+              label="Accessories"
+              className={twMerge(sideBarClass)}
+              renderChevronIcon={(theme, open) => {
+                return open ? (
+                  <HiOutlineMinusSm
+                    size={iconSize}
+                    className={twMerge("ml-1", theme.label.icon.open["on"])}
+                  />
+                ) : (
+                  <HiOutlinePlusSm
+                    size={iconSize}
+                    className={twMerge("ml-1", theme.label.icon.open["off"])}
+                  />
+                );
+              }}
+            >
+              {Accessories.map((accessories) => (
+                <Sidebar.Item
+                  key={accessories.name}
+                  className={`${sideBarClass}`}
+                  href={accessories.route}
+                >
+                  <div className={`${divClass}`}>
+                    {React.createElement(accessories.icon, {
+                      className: classStyle,
+                      size: iconSize,
+                    })}
+                    {accessories.name}
+                  </div>
+                </Sidebar.Item>
+              ))}
+            </Sidebar.Collapse>
+
+            <Sidebar.Collapse
+              label="Automobiles"
+              renderChevronIcon={(theme, open) => {
+                const IconComponent = open ? HiOutlineMinusSm : HiOutlinePlusSm;
+
+                return (
+                  <IconComponent
+                    aria-hidden
+                    className={twMerge(
+                      theme.label.icon.open[open ? "on" : "off"]
+                    )}
+                  />
+                );
+              }}
+            >
+              {AutoMobile.map((automobiles) => (
+                <Sidebar.Item
+                  key={automobiles.name}
+                  className={`${sideBarClass}`}
+                  href={automobiles.route}
+                >
+                  <div className={`${divClass}`}>
+                    {React.createElement(automobiles.icon, {
+                      className: classStyle,
+                      size: iconSize,
+                    })}
+                    {automobiles.name}
+                  </div>
+                </Sidebar.Item>
+              ))}
+            </Sidebar.Collapse>
+
+            <Sidebar.Collapse
+              label="Electronics"
+              renderChevronIcon={(theme, open) => {
+                const IconComponent = open ? HiOutlineMinusSm : HiOutlinePlusSm;
+
+                return (
+                  <IconComponent
+                    aria-hidden
+                    className={twMerge(
+                      theme.label.icon.open[open ? "on" : "off"]
+                    )}
+                  />
+                );
+              }}
+            >
+              {Electronics.map((electronics) => (
+                <Sidebar.Item
+                  key={electronics.name}
+                  className={`${sideBarClass}`}
+                  href={electronics.route}
+                >
+                  <div className={`${divClass}`}>
+                    {React.createElement(electronics.icon, {
+                      className: classStyle,
+                      size: iconSize,
+                    })}
+                    {electronics.name}
+                  </div>
+                </Sidebar.Item>
+              ))}
+            </Sidebar.Collapse>
+
+            <Sidebar.Item className={`${sideBarClass}`} href="#">
+              <div className={`${divClass}`}>
+                <FaSprayCan className={`${classStyle}`} size={iconSize} />
+                Fragrances
+              </div>
+            </Sidebar.Item>
+
+            <Sidebar.Item className={`${sideBarClass}`} href="#">
+              <div className={`${divClass}`}>
+                <GiWoodenChair className={`${classStyle}`} size={iconSize} />
+                Furniture
+              </div>
+            </Sidebar.Item>
+
+            <Sidebar.Item className={`${sideBarClass}`} href="#">
+              <div className="flex items-center">
+                <MdLocalGroceryStore
+                  className={`${classStyle}`}
+                  size={iconSize}
+                />
+                Groceries
+              </div>
+            </Sidebar.Item>
+
+            <Sidebar.Item className={`${sideBarClass}`} href="#">
+              <div className={`${divClass}`}>
+                <IoHome className={`${classStyle}`} size={iconSize} />
+                Home Decoration
+              </div>
+            </Sidebar.Item>
+
+            <Sidebar.Collapse
+              label="Men"
+              renderChevronIcon={(theme, open) => {
+                const IconComponent = open ? HiOutlineMinusSm : HiOutlinePlusSm;
+
+                return (
+                  <IconComponent
+                    aria-hidden
+                    className={twMerge(
+                      theme.label.icon.open[open ? "on" : "off"]
+                    )}
+                  />
+                );
+              }}
+            >
+              {Men.map((men) => (
+                <Sidebar.Item
+                  key={men.name}
+                  className={`${sideBarClass}`}
+                  href={men.route}
+                >
+                  <div className={`${divClass}`}>
+                    {React.createElement(men.icon, {
+                      className: classStyle,
+                      size: iconSize,
+                    })}
+                    {men.name}
+                  </div>
+                </Sidebar.Item>
+              ))}
+            </Sidebar.Collapse>
+
+            <Sidebar.Item className={`${sideBarClass}`} href="#">
+              <div className={`${divClass}`}>
+                <FaHandSparkles className={`${classStyle}`} size={iconSize} />
+                Skincare
+              </div>
+            </Sidebar.Item>
+
+            <Sidebar.Item className={`${sideBarClass}`} href="#">
+              <div className={`${divClass}`}>
+                <IoGlasses className={`${classStyle}`} size={iconSize} />
+                Sunglasses
+              </div>
+            </Sidebar.Item>
+
+            <Sidebar.Collapse
+              label="Women"
+              renderChevronIcon={(theme, open) => {
+                const IconComponent = open ? HiOutlineMinusSm : HiOutlinePlusSm;
+
+                return (
+                  <IconComponent
+                    aria-hidden
+                    className={twMerge(
+                      theme.label.icon.open[open ? "on" : "off"]
+                    )}
+                  />
+                );
+              }}
+            >
+              {Women.map((women) => (
+                <Sidebar.Item
+                  key={women.name}
+                  className={`${sideBarClass}`}
+                  href={women.route}
+                >
+                  <div className={`${divClass}`}>
+                    {React.createElement(women.icon, {
+                      className: classStyle,
+                      size: iconSize,
+                    })}
+                    {women.name}
+                  </div>
+                </Sidebar.Item>
+              ))}
+            </Sidebar.Collapse>
+          </Sidebar.ItemGroup>
+        </Sidebar.Items>
+      </Sidebar>
+      <div>
+        <h1>Shop Page</h1>
       </div>
-    </React.Fragment>
+    </div>
   );
 };
 
