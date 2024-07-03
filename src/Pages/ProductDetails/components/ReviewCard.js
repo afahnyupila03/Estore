d544c04d97f982612bf5fe20cbcf2304c118b68b
Afahnyu Pila <AfahnyuPila03>
2024-07-03 14:03:38 +0200

    remove date format(hr/mins) from product review-cerd component

--- src/Pages/ProductDetails/components/ReviewCard.js
+++ src/Pages/ProductDetails/components/ReviewCard.js
@@ -7,10 +7,8 @@ function formatDate(dateString) {
   const year = date.getFullYear().toString().slice(-2);
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
 
-  return `${year}-${month}-${day} / ${hours}:${minutes}`;
+  return `${year}-${month}-${day}`;
 }
 
 function renderStars(rating) {
